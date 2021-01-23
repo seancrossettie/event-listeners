fc57966f472ab63da0a71da1e0dacf6f96dcc6e1
Sean <jb@JBs-MacBook-Pro.local>
2021-01-23 12:00:03 -0600

    added event listeners

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-console.log('CONNECTED');
+// console.log('CONNECTED');
+
+const button = document.querySelector('#main-button');
+const container = document.querySelector('#container');
+// button.addEventListener('click', (e) => {
+//   console.log(`You clicked ${e.target.id}.`);
+//   console.log(e.isTrusted);
+// })
+
+const whatIsTheId = (e) => {
+  console.log(`You clicked ${e.target.id}`);
+
+  if (e.target.id === 'main-button') {
+    container.innerHTML = 'You clicked the main button';
+  }
+}
+
+button.addEventListener('click', whatIsTheId)
+
+const button2 = document.querySelector(`#other-button`);
+
+button2.addEventListener('click', whatIsTheId);
