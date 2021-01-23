2b65a26a345ced562af5f4155669664da88067c9
Sean <jb@JBs-MacBook-Pro.local>
2021-01-23 11:08:41 -0600

    added callback functions

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
 console.log('CONNECTED');
+
+const sayHi = (user) => {
+  return `Hello, ${user}!`
+}
+
+const sayBye = (user) => {
+  return `Bye, ${user}`
+}
+
+const createGreeting = (user, cb) => {
+  return cb(user);
+}
+
+console.log(createGreeting('Sean', sayHi));
